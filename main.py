93459793bd5193ebe7e96ea63627083d0af21eeb
Plextora <plextora@pm.me>
2022-12-01 18:49:58 -0500

    The presence now includes the game you are currently playing on ROBLOX

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
+import re
 from pypresence import Presence
 import time
 import psutil
 import os
 import platform
+import requests
 
 if platform.system() != "Linux":
     print(
@@ -18,21 +20,52 @@ if platform.system() != "Linux":
     if decision == "n":
         os._exit(1)
 
-
 start_time = time.time()
-client_id = "1044302023211884624"
+client_id = 1044302023211884624
 rpc = Presence(client_id)
 rpc.connect()
 other_process_name = "robloxplayerbet"
 main_process_name = "robloxplayerbeta"
 
 
+def get_current_game():
+    if if_roblox_running():
+        place_id_regex = re.compile(r"placeId=(\d+)")
+        for process in psutil.process_iter():
+            place_id_string = place_id_regex.search(
+                " ".join(process.cmdline()))
+            if place_id_string:
+                if place_id_string != "":
+                    place_id = place_id_string.group().replace("placeId=", "")
+                    # wacky regex shenanigans, I might clean this up in the future but for now im just done with regex.
+
+        response = requests.get(
+            "https://api.roblox.com/marketplace/productinfo?assetId=" + place_id
+        )
+
+        try:
+            return response.json()["Name"]
+        except:
+            if response.status_code == 429:
+                print("Rate limited!")
+            return "Program is being rate limited!"
+
+
 def start_presence():
-    rpc.update(
-        large_image="robloxicon",
-        large_text="ROBLOX Icon",
-        start=start_time,
-    )
+    current_game = get_current_game()
+    if current_game != "Program is being rate limited!":
+        rpc.update(
+            large_image="robloxicon",
+            large_text="ROBLOX Icon",
+            start=start_time,
+            state=f"Playing {current_game}",
+        )
+    else:
+        rpc.update(
+            large_image="robloxicon",
+            large_text="ROBLOX Icon",
+            start=start_time,
+        )
 
 
 def if_roblox_running():
@@ -47,9 +80,7 @@ def if_roblox_running():
     return False
 
 
-start_presence()
-
-print("Started presence!")
+print("Starting presence! The presence will reload every 15 seconds.\nThis is because of ROBLOX's rate limiting on API requests.\n")
 
 while True:
     if if_roblox_running():
@@ -57,5 +88,5 @@ while True:
     else:
         rpc.clear()
         start_time = time.time()  # reset the time elapsed field in rpc
-    time.sleep(1)
+    time.sleep(15)
     print("Reloaded presence!")
